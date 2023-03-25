73ff64b1b546e7a55838165cb83af888a39e1b4a
TanKaka123 <hongtan1422002@sgmail.com>
2023-03-25 11:02:25 +0700

    first code

---
+++ source/styles/constaint.js
@@ -0,0 +1,19 @@
+export default {
+    colors: {
+      primary: '#B05E27',
+      secondary: '#FFD600',
+      success: '#28a745',
+      danger: '#dc3545',
+      warning: '#ffc107',
+      info: '#17a2b8',
+      light: '#f8f9fa',
+      dark: '#343a40',
+      white: '#ffffff',
+      black: '#000000'
+    },
+    fonts: {
+      regular: 'Helvetica',
+      bold: 'Helvetica-Bold',
+      italic: 'Helvetica-Italic'
+    },
+  }
